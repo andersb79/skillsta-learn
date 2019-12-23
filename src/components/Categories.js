0cef69f58239e769502d05dab3d1a0466d103588
Anders Bengtsson <andersb79@hotmail.com>
2019-12-23 13:59:06 +0100

    new stuff

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
 import { observer } from "mobx-react-lite";
 
+const useStyles = makeStyles(theme => ({
+  card: {
+    marginTop: "10px"
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%"
+  }
+}));
+
 function Categories({ store }) {
-  return <div className="game">hej</div>;
+  const classes = useStyles();
+  return (
+    <div className="game">
+      {store.seasons.map((season, i) => (
+        <Card className={classes.card}>
+          <CardHeader
+            title={season.friendlyName}
+            subheader={season.friendlyName}
+          />
+
+          <CardContent>hej</CardContent>
+        </Card>
+      ))}
+    </div>
+  );
 }
 
 export default observer(Categories);
